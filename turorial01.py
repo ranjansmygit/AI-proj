3865f0462787e0490de15cd71a656b656d527d98
ranjansmygit <ranjan.smy@gmail.com>
2024-01-03 16:34:34 +0530

    Tensorflow

--- turorial01.py
+++ turorial01.py
@@ -7,4 +7,13 @@ data = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = data.load_data()
 
-print(train_labels[5])
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+#plt.imshow(train_images[3])
+#plt.show()
+#print(train_labels[5])
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation="softmax")
+    ])
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
